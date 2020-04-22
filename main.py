ac349e6036b408ebf77b404f59a5ee4c3d9a6f3e
The5teve <61376282+The5teve@users.noreply.github.com>
2020-04-22 22:47:50 +0300

    Add files via upload
    Приближение к рабочей версии: жду библиотеку и в скором наладим связь с сервером.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 from tkinter import *
 import tkinter.ttk as ttk
 from tkinter import filedialog
 from tkinter import messagebox
+from PIL  import ImageTk,Image
 root = Tk()
 root.geometry("800x450")
 root.title("AllSafe")
@@ -10,22 +12,74 @@ root.resizable(False, False)
 
 
 def main_def_send_mail():
-	return
+	sends = Toplevel()
+	
+	sends['bg']='#202020'
+	sends.geometry("450x200")
+	sends.resizable(False, False)
+	RANDOM = Label(sends, text='Please wait...', bg="#202020", fg="white")
+	RANDOM.place(x=190,y=30)
+	pb = ttk.Progressbar(sends, length=250)
+	pb.place(x=100, y=50)
+	pb.start(200)
+	#for i in range(2,100):
+	#	RANDOM.config(text=os.urandom(3))
+
+	sends.mainloop()
+
+###
+SetPaths= set()
 def choose_file():
 	global tree1
 	global file_path
+	global SetPaths
+	global Button_send
+	global SetMails
 	file_path =  filedialog.askopenfilename(initialdir = "/",title = "Select file", filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
-	tree1.insert("",0, text=file_path)
+	if len(file_path)>3:
+		tree1.insert("",0, text=os.path.split(file_path)[1], values=(str(os.path.getsize(file_path)/1024//1)+"KB"))
+		SetPaths.add(file_path)
+		if len(SetMails)>0:
+			Button_send.config(state=NORMAL)
+
+def placehelptext(number):
+	print(os.urandom(3))
+	
+	if number == 1:
+		myHelp1=Label(helps, text="The program (desktop / mobile) sends a request to the server.\n Before sending a request It receives a response from the \n server with the public key, encrypts the files using the\nlibrary, and the key sends them to the server along\n with the recipients (lists by e-mail)")		
+		myHelp1.place(x=10,y=15)	
+
+	elif number == 2:
+		myHelp2=Label(helps, text="The file server generates responses to sender requests, generates a\n key pair, sends the public key to the user, receives \nfiles, decrypts and generates download links")
+		myHelp2.place(x=10,y=180)
+
+	else:
+		myHelp3=Label(helps,text="The mail server takes all address and address files, sends\n links  to download files to all addresses. The recipient opens\n the mail, clicks the link, downloads the file")
+		myHelp3.place(x=10,y=320)
+	
+
+
+
 def show_help():
+    global helps
     helps = Toplevel()
-    helps.geometry('200x150')
-    helps['bg'] = '#191919'
-    helps.geometry("400x300")
+    helps['bg'] = '#E6F0ED'
+    helps.geometry("700x410")
+    helps.resizable(False, False)
     #Label(a, text="About this").pack(expand=1)
     helps.grab_set()
     helps.focus_set()
+    imghelp = ImageTk.PhotoImage(Image.open("img/hiw.png"))
+    LabelHelp = Label(helps,image=imghelp)
+    LabelHelp.place(x=180,y=0)
+    helpbutton1=Button(helps,text="What\n is it?",font=("Arial Bold", 13), bg="#E6F0ED", borderwidth=0, fg="grey", command=lambda: placehelptext(1))
+    helpbutton1.place(x=600,y=40)
+    helpbutton2=Button(helps,text="What\n is it?",font=("Arial Bold", 13), bg="#E6F0ED", borderwidth=0, fg="grey", command=lambda: placehelptext(2))
+    helpbutton2.place(x=600,y=180)
+    helpbutton3=Button(helps,text="What\n is it?",font=("Arial Bold", 13), bg="#E6F0ED", borderwidth=0, fg="grey", command=lambda: placehelptext(3))
+    helpbutton3.place(x=600,y=320)
+	
     helps.mainloop()
-
     
 
 def show_f1rst_frame():
@@ -61,7 +115,7 @@ def validcheck():
 	if temp.find("@")==-1 or temp.find(".")==-1:
 		messagebox.showerror("Error", "Wrong mail type.")
 	else:	
-		if temp.find("@") and temp.find(".") and temp.index("@")+1!=temp.index(".") and temp.index(".")!=len(temp)-1 and temp.index("@")<temp.index(".") and temp.count("@")==1:
+		if temp.find("@") and temp.find(".") and temp.index("@")+1!=temp.index(".") and temp.index(".")!=len(temp)-1  and temp.count("@")==1:
 			if temp not in SetMails:
 				tree.insert("",0, text=temp)
 				SetMails.add(temp)
@@ -71,6 +125,10 @@ def validcheck():
 				emailplace.delete(0, END)
 		else:
 			messagebox.showerror("Error", "Wrong email.")		
+	if len(SetPaths)>0:
+		Button_send.config(state=NORMAL)
+
+helps = Label()
 #Frames 
 my_frame = Frame(root, bg="#191919", width=150, height=450)
 my_frame.grid(row=0, column=0, rowspan=10)
@@ -114,9 +172,13 @@ Sending1.place(x=30, y=30)
 #Mail pole
 emailplace= Entry(subframe,width=45,bg="lightgrey", fg="black", borderwidth=0)
 emailplace.place(x=10, y=10)
+
 #addmail Button
 addmail=Button(subframe, text="add mail", bg="#2f2f4f", fg="white", pady=1, command=validcheck, borderwidth=0)
 addmail.place(x=295, y=9)
+
+
+
 #MAILS TREE
 tree=ttk.Treeview(subframe, height=6)
 tree.column("#0",stretch="NO")
@@ -125,10 +187,17 @@ tree.heading("#0",text="e-mails",anchor=W)
 tree.place(x=400, y=5)
 
 tree1=ttk.Treeview(subframe, height=6)
-tree1.column("#0",stretch="NO")
+tree1["columns"]=("one")
+tree1.column("#0",width=150,stretch="NO")
 tree1.heading("#0",text="chosen files",anchor=W)
+tree1.column("one",stretch="NO")
+tree1.heading("one",text=" ", anchor=W)
 #tree.insert("",1, text="your mail here") #for inserting
 tree1.place(x=400, y=155)
+
+
+
+
 
 Sending2= Label(subframe, text="Choose files yow want to send", borderwidth=0, bg="#2f2f4f", fg="#666464")
 Sending2.place(x=10, y=130)
@@ -150,8 +219,8 @@ Button_send.place(x=8, y=255)
 
 
 sec0nd_frame = Frame(root, bg="#202020", width=650, height=450)
-WarningLabel = Label(sec0nd_frame, bg="#202020", text="idk", fg="white")
-WarningLabel.place(x=270, y=150)
+WarningLabel = Label(sec0nd_frame, bg="#202020", text="AllSafe is a free program for the safe \n \ntransfer of mail letters. ", fg="white") 
+WarningLabel.place(x=200, y=180)
 #Some things
 
 
