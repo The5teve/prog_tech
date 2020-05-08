84189caa7f7f350bbd0e086dc0e3eaa2e5eab0f2
The5teve <61376282+The5teve@users.noreply.github.com>
2020-05-08 14:24:43 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from tkinter import *
+from tkinter import * 
 import tkinter.ttk as ttk
 from tkinter import filedialog
 from tkinter import messagebox
@@ -13,6 +13,15 @@ root.geometry("800x450")
 root.title("Allsafe")
 root.resizable(False, False)
 root.iconbitmap('img/allsafe.ico')
+
+
+
+style = ttk.Style()
+style.theme_use("clam")
+style.configure("Treeview", 
+                fieldbackground="#1b2c45", foreground="black", borderwidth=0)
+style.configure("Treeview.Heading", 
+                fieldbackground="red", foreground="black", backgroud="red", borderwidth=0)
 
 def main_def_send_mail():
 	global SetPaths
@@ -55,18 +64,19 @@ def choose_file():
 	global SetPaths
 	global Button_send
 	global SetMails
-	file_path =  filedialog.askopenfilename(initialdir = "/",title = "Select file", filetypes = (("all files","*.*"),("jpeg files","*.jpg")))
+	file_path =  filedialog.askopenfilename(initialdir = "/",
+											title = "Select file", filetypes = (("all files","*.*"),("jpeg files","*.jpg")))
 	if len(file_path)>3:
 		tree1.insert("",0, text=os.path.split(file_path)[1], values=(str(os.path.getsize(file_path)/1024//1)+"KB"))
 		SetPaths.add(file_path)
 		if SetMails:
-			Button_send.config(state=NORMAL)
+			Button_send.config(state=NORMAL, bg="lightgrey")
 
 
 
 
 ###HOW THAT WORKS###
-def placehelptext(number):
+def place_help_text(number):
 	print(os.urandom(3))
 	
 	if number == 1:
@@ -97,16 +107,20 @@ def show_help():
     helps.grab_set()
     helps.focus_set()
     imghelp = ImageTk.PhotoImage(Image.open("img/hiw.png"))
+
     LabelHelp = Label(helps,image=imghelp, borderwidth=0)
     LabelHelp.place(x=350,y=0)
+
     helpbutton1=Button(helps,text="What\n is it?",font=("Arial Bold", 13),
-    				 bg="#B3B3B3", borderwidth=0, fg="grey", command=lambda: placehelptext(1))
+    				 bg="#B3B3B3", borderwidth=0, fg="grey", command=lambda: place_help_text(1))
     helpbutton1.place(x=600,y=50)
+
     helpbutton2=Button(helps,text="What\n is it?",font=("Arial Bold", 13),
-    				 bg="#B3B3B3", borderwidth=0, fg="grey", command=lambda: placehelptext(2))
+    				 bg="#B3B3B3", borderwidth=0, fg="grey", command=lambda: place_help_text(2))
     helpbutton2.place(x=600,y=200)
+
     helpbutton3=Button(helps,text="What\n is it?",font=("Arial Bold", 13),
-    				 bg="#B3B3B3", borderwidth=0, fg="grey", command=lambda: placehelptext(3))
+    				 bg="#B3B3B3", borderwidth=0, fg="grey", command=lambda: place_help_text(3))
     helpbutton3.place(x=600,y=350)
 	
     helps.mainloop()
@@ -134,10 +148,11 @@ def button_home():
 	f1rst_frame.grid_forget()
 	sec0nd_frame.grid_forget()
 	my_frame1.grid(row=0, column=1)
-
+def show_send_letter():
+	return
 #Set for mails
 SetMails= set()
-def validcheck():
+def valid_check():
 	global emailplace
 	global tree
 	global SetMails
@@ -161,7 +176,7 @@ def validcheck():
 		else:
 			messagebox.showerror("Error", "Wrong email.")		
 	if SetPaths:
-		Button_send.config(state=NORMAL)
+		Button_send.config(state=NORMAL, bg="lightgrey")
 
 helps = Label()
 #Frames 
@@ -198,28 +213,36 @@ logoLabel.place(x=100,y=150)
 
 
 #Switch Buttons
-button_switch1 = Button(my_frame, text="Let's send something!",bg="#181818", fg="white", borderwidth=0, command=show_f1rst_frame)
+button_switch1 = Button(my_frame, text="Let's send something!",bg="#181818", fg="lightgrey", borderwidth=0, command=show_f1rst_frame)
 button_switch1.place(x=20, y=100)
 
-button_switch2 = Button(my_frame, text="Show reference.", bg="#181818", fg="white", borderwidth=0, command=show_sec0nd_frame)
+button_switch2 = Button(my_frame, text="Show reference.", bg="#181818", fg="lightgrey", borderwidth=0, command=show_sec0nd_frame)
 button_switch2.place(x=20, y=130)
 
 
 #switching frames
 f1rst_frame = Frame(root, bg="#202020", width=650, height=450)
-subframe = Frame(f1rst_frame, width=610, height=310, bg="#1b2c45")
+subframe = Frame(f1rst_frame, width=610, height=310, bg="#1b2c45",borderwidth=2, relief="ridge")
 subframe.place(x=20, y=60)
-button_addfile = Button(subframe, padx=50, pady=10, text="Choose File", bg="#282828", fg="White", command=choose_file)
+
+logosend = ImageTk.PhotoImage(Image.open("img/logosend.png"))
+logoSendL = Label(subframe,image=logosend, borderwidth=0)
+logoSendL.place(x=220,y=80)
+
+button_addfile = Button(subframe, padx=50, pady=10, text="Choose File", bg="#282828", fg="lightgrey", command=choose_file, borderwidth=0)
 button_addfile.place(x=10,y=80)
+
 Sending1= Label(subframe, text="Enter email here", borderwidth=0, bg="#1b2c45", fg="#666464")
 Sending1.place(x=30, y=30)
 #Mail pole
-emailplace= Entry(subframe,width=45,bg="lightgrey", fg="black", borderwidth=0)
+emailplace= Entry(subframe,width=45,bg="lightgrey", fg="grey", borderwidth=0)
 emailplace.place(x=10, y=10)
 
 #addmail Button
-addmail=Button(subframe, text="add mail", bg="#1b2c45", fg="white", pady=1, command=validcheck, borderwidth=0)
+addmail=Button(subframe, text="add mail", bg="#1b2c45", fg="lightgrey", pady=1, command=valid_check, borderwidth=0)
 addmail.place(x=295, y=9)
+
+
 
 
 #MAILS TREE
@@ -243,7 +266,7 @@ tree1.place(x=400, y=155)
 Sending2= Label(subframe, text="Choose files yow want to send", borderwidth=0, bg="#1b2c45", fg="#666464")
 Sending2.place(x=10, y=130)
 Button_send = Button(subframe,text="Send", padx=30, pady=5,font=("Impact", 11),
-					 bg="#b7bcb9",state=DISABLED, command=main_def_send_mail)
+					 bg="grey",state=DISABLED, command=main_def_send_mail, borderwidth=0)
 Button_send.place(x=8, y=255)
 #SendMessage.place(x=430,y=330)
 #263e4d cиний
