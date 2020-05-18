cb27b649d4b2b40b3b301d29bce433b6d841b025
Steeler1337 <55598393+Steeler1337@users.noreply.github.com>
2020-05-18 22:53:02 +0300

    1-st version on VPS
    1-st version on VPS

--- test.py
+++ test.py
@@ -4,25 +4,64 @@ import codecs
 import socket
 import base64
 import time
+import zipfile
+import os
 
-sock = socket.socket()
-sock.bind(('', 8080))
-sock.listen(1)
-conn, addr = sock.accept()
+un=open("UserNumber.txt","r")
+i = int(un.read())
+un.close()
 
-print ('connected:', addr)
-res=''
 while True:
-    data = conn.recv(4096)
-    if not data:
-        break
-    print(data)
-    b1=data
-    d=base64.b64decode(b1)
-    s2=d.decode("UTF-8")
-    print(s2)
-    #conn.send(s2.encode())
-    print('---------------------------------')
-    conn.send('Данные были успешно отправлены'.encode("UTF-8"))
 
-conn.close()
+    sock = socket.socket()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #потому что на Ubuntu 3 минуты "отдышка" после передачи по сокетам
+    sock.bind(('', 8008)) #8080
+    sock.listen(1)
+    conn, addr = sock.accept()
+
+    print ('connected:', addr)
+
+
+
+
+    numfile = conn.recv(10)
+    numfile = numfile.decode("UTF-8")
+    res=b''
+    i+=1
+    new_zip = zipfile.ZipFile('User' + str(i), 'a')
+    for j in range(int(numfile)):
+        file_name = conn.recv(1024)
+        print(file_name)
+        res = b''
+        while True:
+            #print(file_name)
+            data = conn.recv(4096)
+            if data==b'stop':
+                break
+            if not data:
+                print('no data')
+                break
+            res += data
+
+
+        res = base64.b64decode(res)
+
+        g = open(file_name, "wb")
+        g.write(res)
+        g.close()
+        print("File received and recreated")
+
+        new_zip.write(file_name.decode("UTF-8"))
+
+        #path = os.path.join(os.path.abspath(os.path.dirname(__file__)), file_name.decode("UTF-8"))
+        os.remove(file_name)
+
+        file_name=b''
+        res = b''
+
+    un = open("UserNumber.txt", "w")
+    un.write(str(i))
+    un.close()
+
+    new_zip.close()
+    conn.close()
